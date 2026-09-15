ab8c02dfed604c63c7b69c12aa1bcb7bde8d7e4a
Shubham Singh <shubhammahar1306@gmail.com>
2026-09-15 18:58:41 +0530

    (chore): tutorial on select statement
    Signed-off-by: Shubham Singh <shubhammahar1306@gmail.com>

---
+++ select/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ch := make(chan int)
+	ch2 := make( chan string)
+
+	go func() {
+		ch <- 1
+	}()
+
+	go func() {
+		ch2 <- "shubham"
+	}()
+
+	select {
+	case a := <- ch:
+		fmt.Println(a)
+	case b := <- ch2:
+		fmt.Println(b) 
+	}
+}
